acd3e3f4afc875581f6ec47e699326165949853c
Seniy_Jalilow Seniy_Jalilow <arseniy-3108@mail.ru>
2023-12-06 01:19:31 +0500

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,173 @@
+﻿namespace УП
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnCombin = new System.Windows.Forms.Button();
+            this.btnPlace = new System.Windows.Forms.Button();
+            this.btnPermut = new System.Windows.Forms.Button();
+            this.txtN = new System.Windows.Forms.TextBox();
+            this.txtM = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lblCombin = new System.Windows.Forms.Label();
+            this.lblPlace = new System.Windows.Forms.Label();
+            this.lblPermut = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnCombin
+            // 
+            this.btnCombin.Location = new System.Drawing.Point(16, 12);
+            this.btnCombin.Name = "btnCombin";
+            this.btnCombin.Size = new System.Drawing.Size(103, 43);
+            this.btnCombin.TabIndex = 0;
+            this.btnCombin.Text = "Сочетание";
+            this.btnCombin.UseVisualStyleBackColor = true;
+            this.btnCombin.Click += new System.EventHandler(this.btnCombin_Click);
+            // 
+            // btnPlace
+            // 
+            this.btnPlace.Location = new System.Drawing.Point(172, 12);
+            this.btnPlace.Name = "btnPlace";
+            this.btnPlace.Size = new System.Drawing.Size(103, 43);
+            this.btnPlace.TabIndex = 0;
+            this.btnPlace.Text = "Размещение";
+            this.btnPlace.UseVisualStyleBackColor = true;
+            this.btnPlace.Click += new System.EventHandler(this.btnPlace_Click);
+            // 
+            // btnPermut
+            // 
+            this.btnPermut.Location = new System.Drawing.Point(325, 12);
+            this.btnPermut.Name = "btnPermut";
+            this.btnPermut.Size = new System.Drawing.Size(103, 43);
+            this.btnPermut.TabIndex = 0;
+            this.btnPermut.Text = "Перестановка";
+            this.btnPermut.UseVisualStyleBackColor = true;
+            this.btnPermut.Click += new System.EventHandler(this.btnPermut_Click);
+            // 
+            // txtN
+            // 
+            this.txtN.Location = new System.Drawing.Point(304, 87);
+            this.txtN.Name = "txtN";
+            this.txtN.Size = new System.Drawing.Size(84, 20);
+            this.txtN.TabIndex = 1;
+            this.txtN.TextChanged += new System.EventHandler(this.txtN_TextChanged);
+            // 
+            // txtM
+            // 
+            this.txtM.Location = new System.Drawing.Point(85, 86);
+            this.txtM.Name = "txtM";
+            this.txtM.Size = new System.Drawing.Size(84, 20);
+            this.txtM.TabIndex = 1;
+            this.txtM.TextChanged += new System.EventHandler(this.txtM_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(284, 86);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(14, 16);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "n";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(52, 87);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(18, 16);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "m";
+            // 
+            // lblCombin
+            // 
+            this.lblCombin.AutoSize = true;
+            this.lblCombin.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.lblCombin.Location = new System.Drawing.Point(139, 25);
+            this.lblCombin.Name = "lblCombin";
+            this.lblCombin.Size = new System.Drawing.Size(0, 24);
+            this.lblCombin.TabIndex = 3;
+            // 
+            // lblPlace
+            // 
+            this.lblPlace.AutoSize = true;
+            this.lblPlace.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.lblPlace.Location = new System.Drawing.Point(281, 25);
+            this.lblPlace.Name = "lblPlace";
+            this.lblPlace.Size = new System.Drawing.Size(0, 24);
+            this.lblPlace.TabIndex = 3;
+            // 
+            // lblPermut
+            // 
+            this.lblPermut.AutoSize = true;
+            this.lblPermut.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.lblPermut.Location = new System.Drawing.Point(434, 25);
+            this.lblPermut.Name = "lblPermut";
+            this.lblPermut.Size = new System.Drawing.Size(0, 24);
+            this.lblPermut.TabIndex = 3;
+            this.lblPermut.Click += new System.EventHandler(this.lblPermut_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(500, 145);
+            this.Controls.Add(this.lblPermut);
+            this.Controls.Add(this.lblPlace);
+            this.Controls.Add(this.lblCombin);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtM);
+            this.Controls.Add(this.txtN);
+            this.Controls.Add(this.btnPermut);
+            this.Controls.Add(this.btnPlace);
+            this.Controls.Add(this.btnCombin);
+            this.Name = "Form1";
+            this.Text = "Элементы комбинаторики";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnCombin;
+        private System.Windows.Forms.Button btnPlace;
+        private System.Windows.Forms.Button btnPermut;
+        private System.Windows.Forms.TextBox txtN;
+        private System.Windows.Forms.TextBox txtM;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lblCombin;
+        private System.Windows.Forms.Label lblPlace;
+        private System.Windows.Forms.Label lblPermut;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace УП
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        //метод Факториала (перестановки)
+        private int Factorial(int n)
+        {
+            int result = 1;
+            // Цикл от 1 до n для вычисления факториала числа n
+            for (int i = 1; i <= n; i++)
+            {
+                result *= i; // Накапливание произведения чисел от 1 до n
+            }
+            return result; // Возвращается результат вычисления факториала
+        }
+        //метод размещения
+        // Метод размещения: вычисляет число размещений из n элементов по m
+        private int Place(int n, int m)
+        {
+            int numerator = Factorial(n); // Числитель - факториал n
+            int denominator = Factorial(n - m); // Знаменатель - факториал (n - m)
+            return numerator / denominator; // Возвращается результат деления
+        }
+        // Метод Сочетания: вычисляет число сочетаний из n элементов по m
+        private int Combination(int n, int m)
+        {
+            int numerator = Factorial(n); // Числитель - факториал n
+            int denominator = Factorial(n - m) * Factorial(m); // Знаменатель - факториал (n - m) * факториал m
+            return numerator / denominator; // Возвращается результат деления
+        }
+        private int n, m;
+        //кнопка Сочетание
+        // Обработчик нажатия кнопки "Сочетание"
+        private void btnCombin_Click(object sender, EventArgs e)
+        {
+            // Проверка корректности введенных данных перед вычислением
+            if (!check()) return;
+            // Получение данных из текстовых полей
+            int n = int.Parse(txtN.Text);
+            int m = int.Parse(txtM.Text);
+            int result = Combination(n, m); // Вычисление числа сочетаний
+                                            // Отображение результата на форме
+            lblCombin.Text = result.ToString();
+            ClearFields(); // Очистка полей ввода
+        }
+        // Обработчик нажатия кнопки "Размещение"
+        private void btnPlace_Click(object sender, EventArgs e)
+        {
+            if (!check()) return;
+            int n = int.Parse(txtN.Text);
+            int m = int.Parse(txtM.Text);
+            int result = Place(n, m); // Вычисление числа размещений
+            lblPlace.Text = result.ToString(); // Отображение результата на форме
+            ClearFields(); // Очистка полей ввода
+        }
+        // Обработчик нажатия кнопки "Перестановка"
+        private void btnPermut_Click(object sender, EventArgs e)
+        {
+            if (!checkFac()) return;
+            int n = int.Parse(txtN.Text);
+            int result = Factorial(n); // Вычисление факториала числа n (перестановки)
+            lblPermut.Text = result.ToString(); // Отображение результата на форме
+            ClearFields(); // Очистка полей ввода
+        }
+        // Метод для очистки текстовых полей
+        private void ClearFields()
+        {
+            txtN.Text = "";
+            txtM.Text = "";
+        }
+        // Обработчик изменения текста в поле ввода для n
+        private void txtN_TextChanged(object sender, EventArgs e)
+        {
+            txtN.MaxLength = 2; // Максимальное количество символов в поле
+            if (!int.TryParse(txtN.Text, out int number)) // Проверка, является ли введенный текст числом
+            {
+                txtN.Text = ""; // Очистка поля, если введенный текст не является числом
+            }
+            else if (number > 15 || number < 1) // Проверка диапазона значений
+            {
+                MessageBox.Show("Введите число от 1 до 15 "); // Вывод сообщения об ошибке
+                txtN.Text = ""; // Очистка поля
+            }
+        }
+        // Обработчик изменения текста в поле ввода для m
+        private void txtM_TextChanged(object sender, EventArgs e)
+        {
+            txtM.MaxLength = 2; // Максимальное количество символов в поле
+            if (!int.TryParse(txtM.Text, out int number)) // Проверка, является ли введенный текст числом
+            {
+                txtM.Text = ""; // Очистка поля, если введенный текст не является числом
+            }
+            else if (number > 15 || number < 1) // Проверка диапазона значений
+            {
+                MessageBox.Show("Число должно быть от 1 до 15."); // Вывод сообщения об ошибке
+                txtM.Text = ""; // Очистка поля
+            }
+        }
+        // Метод для проверки заполненности полей n и m перед вычислением
+        private bool check()
+        {
+            if (string.IsNullOrWhiteSpace(txtN.Text) || string.IsNullOrWhiteSpace(txtM.Text))
+            {
+                MessageBox.Show("Поля не должны быть пустыми", "Ошибка"); // Вывод сообщения об ошибке, если поля пустые
+                return false;
+            }
+            return true;
+        }
+
+        private void lblPermut_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        // Метод для проверки заполненности поля n перед вычислением факториала
+        private bool checkFac()
+        {
+            if (string.IsNullOrWhiteSpace(txtN.Text))
+            {
+                MessageBox.Show("Поля не должны быть пустыми", "Ошибка"); // Вывод сообщения об ошибке, если поле пустое
+                return false;
+            }
+            return true;
+        }
+
+    }
+}
+
